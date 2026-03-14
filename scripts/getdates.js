14bd65df25eaeea0ac9736c825960f156ff762a3
Imolajooji <imolajooji@gmail.com>
2026-03-14 20:55:37 +0100

    commit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,5 @@
 // Dynamically insert current year
-const yearSpan = document.getElementById("currentyear");
-yearSpan.textContent = new Date().getFullYear();
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Dynamically insert last modified date
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
